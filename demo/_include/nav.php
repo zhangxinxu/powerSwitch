d9e7055d02033df85ccfa90d253d24b6be290876
zhangxinxu <zhangxinxu@zhangxinxu.com>
2013-11-07 22:15:35 +0800

    API document
    API document

--- demo/_include/nav.php
+++ demo/_include/nav.php
@@ -15,14 +15,14 @@
             <img class="l" src="../index_logo.gif" alt="张鑫旭-鑫空间-鑫生活" />
         </a>
         <span class="zxx_author_time">by <span role="author">zhangxinxu</span> <span role="timer">2013-10-31 19:46</span></span>
-        <a href="http://www.zhangxinxu.com/wordpress/?p=3758#API" class="zxx_api" title="访问插件API使用">API</a>
+        <a href="../api/index.html" class="zxx_api" title="访问插件API使用">API</a>
     </div>
 </div>
 
 <div class="zxx_nav">
 	<div class="zxx_constr">
     	<ul class="zxx_nav_ul">
-        	<li class="zxx_nav_li"><a href="./" class="zxx_nav_a<?php echo $arrClass[0]; ?>">首页</a></li>
+        	<li class="zxx_nav_li"><a href="index.php" class="zxx_nav_a<?php echo $arrClass[0]; ?>">首页</a></li>
             <li class="zxx_nav_li"><a href="xunlei.php" class="zxx_nav_a<?php echo $arrClass[1]; ?>">迅雷动漫</a></li>
             <li class="zxx_nav_li"><a href="taobao.php" class="zxx_nav_a<?php echo $arrClass[2]; ?>">淘宝网</a></li>
             <li class="zxx_nav_li"><a href="jd.php" class="zxx_nav_a<?php echo $arrClass[3]; ?>">京东商城</a></li>
