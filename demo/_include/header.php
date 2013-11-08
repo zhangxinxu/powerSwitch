fe82b91df192f0e93ebcdcf424468b8e869c1bd7
zhangxinxu <zhangxinxu@zhangxinxu.com>
2013-11-08 10:57:17 +0800

    demo that in form of  '.html' generate
    demo that in form of  '.html' generate

--- demo/_include/header.php
+++ demo/_include/header.php
@@ -4,7 +4,6 @@
 		$ie = $_GET['ie'];
 	}	
 ?>
-
 <meta charset="utf-8">
 <meta http-equiv="x-ua-compatible" content="IE=<?php echo $ie; ?>" />
 <meta http-equiv="description" content="jQuery万能切换插件powerSwitch实例页面之<?php echo $key; ?> » 张鑫旭-鑫空间-鑫生活" />
@@ -12,4 +11,4 @@
 <meta name="keywords" content="powerSwitch, jQuery, 插件" />
 <meta name="author" content="张鑫旭, zhangxinxu" />
 <title>jQuery万能切换插件powerSwitch实例页面之<?php echo $key; ?> » 张鑫旭-鑫空间-鑫生活</title>
-<link rel="stylesheet" href="css/common.css" />
+<link rel="stylesheet" href="./css/common.css" />
